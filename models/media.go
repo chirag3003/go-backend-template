c86dd3426e02d52eecdf19214a6fc31eeee759a8
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-05-05 01:49:36 +0530

    added format command

--- models/media.go
+++ models/media.go
@@ -1,10 +1,10 @@
 package models
 
 type Media struct {
-  ID string `json:"id,omitempty" bson:"_id,omitempty"`
-  Key string `json:"key" bson:"key"`
-  Etag string `json:"etag" bson:"etag"`
-  Size int64 `json:"size" bson:"size"`
-  Mime string `json:"mime" bson:"mime"`
-  Url string `json:"url" bson:"url"`
+	ID   string `json:"id,omitempty" bson:"_id,omitempty"`
+	Key  string `json:"key" bson:"key"`
+	Etag string `json:"etag" bson:"etag"`
+	Size int64  `json:"size" bson:"size"`
+	Mime string `json:"mime" bson:"mime"`
+	Url  string `json:"url" bson:"url"`
 }
