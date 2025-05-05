00577f669f26141816cd04880aa7636fbb2c9618
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-05-05 23:18:54 +0530

    added proper instructions

--- middlewares/file.go
+++ middlewares/file.go
@@ -26,15 +26,25 @@ func UploadFiles(ctx fiber.Ctx) error {
 			return ctx.SendStatus(fiber.StatusInternalServerError)
 		}
 
-		// open file reader
-		// open, err := file.Open()
+		// If image optimization is required, use the OptimiseImage helper function
 		open, err := helpers.OptimiseImage(*file)
 		if err != nil {
+			// Return an internal server error status if optimization fails
 			return ctx.SendStatus(fiber.StatusInternalServerError)
 		}
 
+		// Uncomment the following lines if image optimization is not required
+		// This will directly open the file without any modifications
+		// open, err := file.Open()
+		// if err != nil {
+		//     // Return an internal server error status if file opening fails
+		//     return ctx.SendStatus(fiber.StatusInternalServerError)
+		// }
+		// defer open.Close() // Ensure the file reader is closed after use
+
+
 		// Creating file key
-		key := fmt.Sprintf("%s/%s%s", os.Getenv("S3_FOLDER"), name, file.Filename + ".webp")
+		key := fmt.Sprintf("%s/%s%s", os.Getenv("S3_FOLDER"), name, file.Filename+".webp")
 
 		// upload file to s3
 		res, err := repo.S3.Upload(ctx.Context(), key, open)
